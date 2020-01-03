cb4b0a6d9b34fe150491677192a077d9a863f82d
Toni Polanec <toni.polanec@gmail.com>
2020-01-03 12:02:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+
+    <style>
+
+    </style>
+
+
+	<body>
+
+    <header>
+
+        <div class="flex-header">
+            <div style="flex-grow: 4; padding:10px;"> <img style="max-height: 80px;" src="img/logo.png" alt="Logo"> </div>
+            <div style="flex-grow: 12"></div>
+            <div style="flex-grow: 1; display: flex;" > <button id="buttonPrijava">Prijava</button></div>
+            <div style="flex-grow: 1; display: flex;"><button id="buttonKontakt">Kontakt</button></div>
+
+        </div>
+
+    </header>
+
+    <navbar>
+        <div class="flex-navbar">
+            <div style="flex-grow: 2">Kategorije(dropdown)</div>
+            <div style="flex-grow: 5">Pretrazi</div>
+            <div style="flex-grow: 1">Kosarica(0.00kn)</div>
+        </div>
+    </navbar>
+
+    <div id="main">
+
+        <div>
+            <div><img class="picProizvod" src="img/SonyWI-C310Wirelessin-Ear.jpg" alt="Sony WI-C310 Wireless in-Ear"></div>
+            <div>Sony WI-C310 Wireless in-Ear</div>
+            <p class="cijena">499.00kn </p>
+        </div>
+        <div>
+            <div><img  class="picProizvod" src="img/HPPavilionGaming300.jpg" alt="HP Pavillion Gamming 300"></div>
+            <div>HP Pavillion Gamming 300</div>
+        </div>
+        <div>
+            <div><img  class="picProizvod" src="img/LogitechMXAnywhere2.jpg" alt="Logitech MX Anywhere 2"></div>
+            <div>Logitech MX Anywhere 2</div>
+        </div>
+        <div>
+            <div><img  class="picProizvod" src="img/BeatsProOver-Ear.jpg" alt="Beats Pro Over-Ear"></div>
+            <div>Beats Pro Over-Ear</div>
+        </div>
+        <div>
+            <div><img  class="picProizvod" src="img/BoseCompanion2.jpg" alt="Bose Companion 2"></div>
+            <div>Bose Companion 2</div>
+        </div>
+        <div>
+            <div><img  class="picProizvod" src="img/FireStormMech.jpg" alt="FireStorm Mech"></div>
+            <div>FireStorm Mech</div>
+        </div>
+
+
+
+
+
+    </div>
+
+    <footer>
+        <div>
+            <p></p>
+        </div>
+    </footer>
+
+
+    </body>
+
+</html>
